3059ea94b565c5709f5efcfe418b1fbbfd89ae79
PayalJainC <payal@moonraft.com>
2018-07-22 11:34:36 +0530

    upload

---
+++ responsive_webpage_dev/js/dropdown.js
@@ -0,0 +1,39 @@
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("dashboard");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+  
+
+ /*function myFunction() {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if(dropdowms[i] != document.getElementById() ){
+       openDropdown.classList.remove('show');
+                        }
+                }
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} */
